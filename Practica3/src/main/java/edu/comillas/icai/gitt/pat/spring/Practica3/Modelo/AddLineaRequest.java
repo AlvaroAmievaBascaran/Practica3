2414773c1b746feb975016aa7af9631af9749202
Alvaro <alvaroamievabascaran@gmail.com>
2026-03-05 13:35:11 +0100

    Detalles

--- Practica3/src/main/java/edu/comillas/icai/gitt/pat/spring/Practica3/Modelo/AddLineaRequest.java
+++ Practica3/src/main/java/edu/comillas/icai/gitt/pat/spring/Practica3/Modelo/AddLineaRequest.java
@@ -2,7 +2,6 @@ package edu.comillas.icai.gitt.pat.spring.Practica3.Modelo;
 
 public class AddLineaRequest {
     public Long idArticulo;
-    public String descripcion;
     public double precioUnitario;
     public int unidades;
 }
