7496f179a8fa4d41c4d53bf414379a95d8f9f542
Alvaro <alvaroamievabascaran@gmail.com>
2026-03-05 12:46:58 +0100

    Detalle

--- src/main/java/edu/comillas/icai/gitt/pat/spring/Practica3/Repositorio/LineaCarritoRepositorio.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/Practica3/Repositorio/LineaCarritoRepositorio.java
@@ -4,4 +4,4 @@ import edu.comillas.icai.gitt.pat.spring.Practica3.Entidades.LineaCarrito;
 import edu.comillas.icai.gitt.pat.spring.Practica3.Entidades.LineaCarritoId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LineaCarritoRepositorio extends JpaRepository<LineaCarrito, LineaCarritoId> {}
+public interface LineaCarritoRepositorio extends JpaRepository<LineaCarrito, long> {}
